dec4ef2737141342c7bd67d27993521208450ce1
Erika Pauwels <erika.pauwels@gmail.com>
2017-12-31 17:50:36 +0100

    Set a sensible default for page size

--- src/Rollvolet.CRM.Domain/Models/Query/PageQuery.cs
+++ src/Rollvolet.CRM.Domain/Models/Query/PageQuery.cs
@@ -3,7 +3,7 @@ namespace Rollvolet.CRM.Domain.Models.Query
     public class PageQuery
     {
         public int Number { get; set; } = 0;
-        public int Size { get; set; } = -1;
+        public int Size { get; set; } = 100;
 
         public int Skip
         {
