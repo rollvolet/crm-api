992abbf77805a3dbdbc2119639f396bdc2356b35
Erika Pauwels <erika.pauwels@gmail.com>
2018-03-21 10:36:48 +0100

    Update issuer validation URI for Azure AD v2

--- src/Rollvolet.CRM.API/Startup.cs
+++ src/Rollvolet.CRM.API/Startup.cs
@@ -79,7 +79,7 @@ namespace Rollvolet.CRM.API
                 }).AddJwtBearer(
                 jwtOptions => {
                     jwtOptions.Audience = Configuration["Authentication:ClientId"];
-                    jwtOptions.Authority = Configuration["Authentication:BaseUri"] + "/" + Configuration["Authentication:TenantId"];
+                    jwtOptions.Authority = Configuration["Authentication:BaseUri"] + "/" + Configuration["Authentication:TenantId"] + "/v2.0";
                     jwtOptions.Events = new JwtBearerEvents
                     {
                         // OnAuthenticationFailed = AuthenticationFailed
