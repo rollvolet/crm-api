0b9f1f95121a7fe3b0f6d70004f63c30d31904c8
Erika Pauwels <erika.pauwels@gmail.com>
2020-05-09 21:14:45 +0200

    Add custom deserializer for IRelationship

--- src/Rollvolet.CRM.APIContracts/JsonApi/IRelationshipConverter.cs
+++ src/Rollvolet.CRM.APIContracts/JsonApi/IRelationshipConverter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -11,7 +12,52 @@ namespace Rollvolet.CRM.APIContracts.JsonApi
 
         public override IRelationship Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            throw new InvalidOperationException();
+            IRelationship relationship = null;
+
+            if (reader.TokenType != JsonTokenType.StartObject)
+            {
+                // incoming JSON-string is not an object. No idea how to deserialize to an IRelationship object in that case.
+                throw new JsonException();
+            }
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndObject)
+                {
+                    // we reached the end of the relationship JSON-string. Let's return the deserialized object.
+                    return relationship;
+                }
+
+                if (reader.TokenType == JsonTokenType.PropertyName)
+                {
+                    var propertyName = reader.GetString();
+                    if (propertyName == "data")
+                    {
+                        if (typeToConvert == typeof(OneRelationship))
+                        {
+                            var data = JsonSerializer.Deserialize<RelatedResource>(ref reader, options);
+                            relationship = new OneRelationship { Data = data };
+                        }
+                        else if (typeToConvert == typeof(ManyRelationship))
+                        {
+                            var data = JsonSerializer.Deserialize<IEnumerable<RelatedResource>>(ref reader, options);
+                            relationship = new ManyRelationship { Data = data };
+                        }
+                        else
+                        {
+                            // Is there an implementation of the IRelationship interface not listed here?
+                            throw new JsonException();
+                        }
+                    }
+                    else
+                    {
+                        // we're only interested in the 'data' property of a relationship for incoming requests
+                        reader.Skip();
+                    }
+                }
+            }
+
+            throw new JsonException();
         }
 
         public override void Write(Utf8JsonWriter writer, IRelationship relationship, JsonSerializerOptions options)
