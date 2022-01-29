1d7c9f9fc71b0af769082b853bbec947988b8b0d
Erika Pauwels <erika.pauwels@gmail.com>
2022-01-29 17:31:29 +0100

    Remove unused DTO classes

--- src/Rollvolet.CRM.APIContracts/DTO/AuthenticationTokens/LoginRequestDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Rollvolet.CRM.APIContracts.DTO.AuthenticationTokens
-{
-    public class LoginRequestDto
-    {
-        public string AuthorizationCode { get; set; }
-        public string RedirectUri { get; set; }
-        public string Scope { get; set; }
-    }
-}
